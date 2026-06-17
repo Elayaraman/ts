61cc5b4636946465cf3e0f277c5d21f3c90df312
Elayaraman <elayaraman007@gmail.com>
2026-06-17 17:39:25 +0530

    Update function.ts

---
+++ src/function.ts
@@ -0,0 +1,21 @@
+function test(ar1: string,ar2?:number ): number{
+    return +ar1;
+}
+
+function addAll(...nums:number[]):number{
+ return nums.reduce((acc,cur)=>acc+cur, 0);
+}
+
+const ff = function (){
+
+}
+
+// :void means it doesn't return anything or return undefined
+
+function log(val:string):never{
+
+    setInterval(()=>{
+        console.log(val);
+    },1000);
+
+}
