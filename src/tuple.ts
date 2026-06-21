db84f82c77290c8729c077d7497d07819dce68f1
Elayaraman <elayaraman007@gmail.com>
2026-06-21 14:15:21 +0530

    tuple

---
+++ src/tuple.ts
@@ -0,0 +1,5 @@
+// tuple is fixed length array we can assign types to the values;
+
+type userTuple=  readonly [number,string];
+let userOne : userTuple = [1,"ram"];
+// let useTwo : userTuple = ["1","ram"];
